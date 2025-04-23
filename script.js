dd46f8ab86821398b2ea40145de2e4e388ff8ac2
ved-birla <vedbirla@berkeley.edu>
2025-04-22 17:48:10 -0700

    updated skeleton
    finished nav bar and footer v1, and cleaned code

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('header.html')
+.then(res => res.text())
+.then(data => document.getElementById('header').innerHTML = data);
+
+fetch('footer.html')
+.then(res => res.text())
+.then(data => document.getElementById('footer').innerHTML = data);
