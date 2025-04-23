30c2a1ddd010fd743901188eab7c73a7ae5c2921
ved-birla <vedbirla@berkeley.edu>
2025-04-22 17:58:05 -0700

    troubleshooting link v1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-fetch('header.html')
+fetch('./header.html')
 .then(res => res.text())
 .then(data => document.getElementById('header').innerHTML = data);
 
-fetch('footer.html')
+fetch('./footer.html')
 .then(res => res.text())
 .then(data => document.getElementById('footer').innerHTML = data);
